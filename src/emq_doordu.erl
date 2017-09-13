60bc693487f5f6275a8985644a2fd9e9c4a1a441
jipeng <jipeng@doordu.com>
2017-09-13 16:43:46 +0800

    debug

--- src/emq_doordu.erl
+++ src/emq_doordu.erl
@@ -34,20 +34,20 @@
 
 %% Called when the plugin application start
 load(Env) ->
-    emqttd:hook('client.connected', fun ?MODULE:on_client_connected/3, [Env]).
-    %% emqttd:hook('client.disconnected', fun ?MODULE:on_client_disconnected/3, [Env]),
-    %% emqttd:hook('client.subscribe', fun ?MODULE:on_client_subscribe/4, [Env]),
-    %% emqttd:hook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4, [Env]),
-    %% emqttd:hook('session.created', fun ?MODULE:on_session_created/3, [Env]),
-    %% emqttd:hook('session.subscribed', fun ?MODULE:on_session_subscribed/4, [Env]),
-    %% emqttd:hook('session.unsubscribed', fun ?MODULE:on_session_unsubscribed/4, [Env]),
-    %% emqttd:hook('session.terminated', fun ?MODULE:on_session_terminated/4, [Env]),
-    %% emqttd:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]),
-    %% emqttd:hook('message.delivered', fun ?MODULE:on_message_delivered/4, [Env]),
-    %% emqttd:hook('message.acked', fun ?MODULE:on_message_acked/4, [Env]).
+    emqttd:hook('client.connected', fun ?MODULE:on_client_connected/3, [Env]),
+    emqttd:hook('client.disconnected', fun ?MODULE:on_client_disconnected/3, [Env]),
+    emqttd:hook('client.subscribe', fun ?MODULE:on_client_subscribe/4, [Env]),
+    emqttd:hook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4, [Env]),
+    emqttd:hook('session.created', fun ?MODULE:on_session_created/3, [Env]),
+    emqttd:hook('session.subscribed', fun ?MODULE:on_session_subscribed/4, [Env]),
+    emqttd:hook('session.unsubscribed', fun ?MODULE:on_session_unsubscribed/4, [Env]),
+    emqttd:hook('session.terminated', fun ?MODULE:on_session_terminated/4, [Env]),
+    emqttd:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]),
+    emqttd:hook('message.delivered', fun ?MODULE:on_message_delivered/4, [Env]),
+    emqttd:hook('message.acked', fun ?MODULE:on_message_acked/4, [Env]).
 
 on_client_connected(ConnAck, Client = #mqtt_client{client_id = ClientId}, _Env) ->
-    %%io:format("client ~s connected, connack: ~w~n", [ClientId, ConnAck]),
+    %% io:format("client ~s connected, connack: ~w~n", [ClientId, ConnAck]),
     lager:info("client ~s connected, connack: ~w~n", [ClientId, ConnAck]),
     {ok, Client}.
 
@@ -82,7 +82,8 @@ on_message_publish(Message = #mqtt_message{topic = <<"$SYS/", _/binary>>}, _Env)
     {ok, Message};
 
 on_message_publish(Message, _Env) ->
-    io:format("publish ~s~n", [emqttd_message:format(Message)]),
+    %% io:format("publish ~s~n", [emqttd_message:format(Message)]),
+    lager:info("publish ~s~n", [emqttd_message:format(Message)]),
     {ok, Message}.
 
 on_message_delivered(ClientId, Username, Message, _Env) ->
@@ -95,15 +96,15 @@ on_message_acked(ClientId, Username, Message, _Env) ->
 
 %% Called when the plugin application stop
 unload() ->
-    emqttd:unhook('client.connected', fun ?MODULE:on_client_connected/3).
-    %% emqttd:unhook('client.disconnected', fun ?MODULE:on_client_disconnected/3),
-    %% emqttd:unhook('client.subscribe', fun ?MODULE:on_client_subscribe/4),
-    %% emqttd:unhook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4),
-    %% emqttd:unhook('session.created', fun ?MODULE:on_session_created/3),
-    %% emqttd:unhook('session.subscribed', fun ?MODULE:on_session_subscribed/4),
-    %% emqttd:unhook('session.unsubscribed', fun ?MODULE:on_session_unsubscribed/4),
-    %% emqttd:unhook('session.terminated', fun ?MODULE:on_session_terminated/4),
-    %% emqttd:unhook('message.publish', fun ?MODULE:on_message_publish/2),
-    %% emqttd:unhook('message.delivered', fun ?MODULE:on_message_delivered/4),
-    %% emqttd:unhook('message.acked', fun ?MODULE:on_message_acked/4).
+    emqttd:unhook('client.connected', fun ?MODULE:on_client_connected/3),
+    emqttd:unhook('client.disconnected', fun ?MODULE:on_client_disconnected/3),
+    emqttd:unhook('client.subscribe', fun ?MODULE:on_client_subscribe/4),
+    emqttd:unhook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4),
+    emqttd:unhook('session.created', fun ?MODULE:on_session_created/3),
+    emqttd:unhook('session.subscribed', fun ?MODULE:on_session_subscribed/4),
+    emqttd:unhook('session.unsubscribed', fun ?MODULE:on_session_unsubscribed/4),
+    emqttd:unhook('session.terminated', fun ?MODULE:on_session_terminated/4),
+    emqttd:unhook('message.publish', fun ?MODULE:on_message_publish/2),
+    emqttd:unhook('message.delivered', fun ?MODULE:on_message_delivered/4),
+    emqttd:unhook('message.acked', fun ?MODULE:on_message_acked/4).
 
