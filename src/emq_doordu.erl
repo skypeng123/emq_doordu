d2f5cbd77a5349b43ab4871805545080245d27e1
jipeng <jipeng@doordu.com>
2017-09-14 14:02:23 +0800

    debug

--- src/emq_doordu.erl
+++ src/emq_doordu.erl
@@ -84,7 +84,9 @@ on_message_publish(Message = #mqtt_message{topic = <<"$SYS/", _/binary>>}, _Env)
 on_message_publish(Message, _Env) ->
     io:format("publish ~s~n", [emqttd_message:format(Message)]),
     Payload = Message#mqtt_message.payload,
+    PayloadData = emq_lwm2m_json:json_to_tlv(Payload),
     io:format("Payload ~s~n", [Payload]),
+    io:format("PayloadData ~s~n", [PayloadData]),
     {ok, Message}.
 
 on_message_delivered(ClientId, Username, Message, _Env) ->
