9adcaab883e6614de7b1df1ae34e2f2ea995a350
jipeng <jipeng@doordu.com>
2017-09-14 10:29:50 +0800

    debug

--- src/emq_doordu.erl
+++ src/emq_doordu.erl
@@ -47,33 +47,30 @@ load(Env) ->
     emqttd:hook('message.acked', fun ?MODULE:on_message_acked/4, [Env]).
 
 on_client_connected(ConnAck, Client = #mqtt_client{client_id = ClientId}, _Env) ->
-    io:format("client ~s connected, connack: ~w~n", [ClientId, ConnAck]),
-    lager:error("client ~s connected, connack: ~w~n", [ClientId, ConnAck]),
+    lager:info("client ~s connected, connack: ~w~n", [ClientId, ConnAck]),
     {ok, Client}.
 
 on_client_disconnected(Reason, _Client = #mqtt_client{client_id = ClientId}, _Env) ->
-    io:format("client ~s disconnected, reason: ~w~n", [ClientId, Reason]),
+    lager:info("client ~s disconnected, reason: ~w~n", [ClientId, Reason]),
     ok.
 
 on_client_subscribe(ClientId, Username, TopicTable, _Env) ->
-    io:format("client(~s/~s) will subscribe: ~p~n", [ClientId, Username, TopicTable]),
-    lager:error("client(~s/~s) will subscribe: ~p~n", [ClientId, Username, TopicTable]),
+    lager:info("client(~s/~s) will subscribe: ~p~n", [ClientId, Username, TopicTable]),
     {ok, TopicTable}.
     
 on_client_unsubscribe(ClientId, Username, TopicTable, _Env) ->
-    io:format("client(~s/~s) unsubscribe ~p~n", [ClientId, Username, TopicTable]),
+    lager:info("client(~s/~s) unsubscribe ~p~n", [ClientId, Username, TopicTable]),
     {ok, TopicTable}.
 
 on_session_created(ClientId, Username, _Env) ->
-    io:format("session(~s/~s) created.", [ClientId, Username]).
+    lager:info("session(~s/~s) created.", [ClientId, Username]).
 
 on_session_subscribed(ClientId, Username, {Topic, Opts}, _Env) ->
-    io:format("session(~s/~s) subscribed: ~p~n", [ClientId, Username, {Topic, Opts}]),
-    lager:error("session(~s/~s) subscribed: ~p~n", [ClientId, Username, {Topic, Opts}]),
+    lager:info("session(~s/~s) subscribed: ~p~n", [ClientId, Username, {Topic, Opts}]),
     {ok, {Topic, Opts}}.
 
 on_session_unsubscribed(ClientId, Username, {Topic, Opts}, _Env) ->
-    io:format("session(~s/~s) unsubscribed: ~p~n", [ClientId, Username, {Topic, Opts}]),
+    lager:info("session(~s/~s) unsubscribed: ~p~n", [ClientId, Username, {Topic, Opts}]),
     ok.
 
 on_session_terminated(ClientId, Username, Reason, _Env) ->
@@ -84,15 +81,15 @@ on_message_publish(Message = #mqtt_message{topic = <<"$SYS/", _/binary>>}, _Env)
     {ok, Message};
 
 on_message_publish(Message, _Env) ->
-    io:format("publish ~s~n", [emqttd_message:format(Message)]),
+    lager:info("publish ~s~n", [emqttd_message:format(Message)]),
     {ok, Message}.
 
 on_message_delivered(ClientId, Username, Message, _Env) ->
-    io:format("delivered to client(~s/~s): ~s~n", [ClientId, Username, emqttd_message:format(Message)]),
+    lager:info("delivered to client(~s/~s): ~s~n", [ClientId, Username, emqttd_message:format(Message)]),
     {ok, Message}.
 
 on_message_acked(ClientId, Username, Message, _Env) ->
-    io:format("client(~s/~s) acked: ~s~n", [ClientId, Username, emqttd_message:format(Message)]),
+    lager:info("client(~s/~s) acked: ~s~n", [ClientId, Username, emqttd_message:format(Message)]),
     {ok, Message}.
 
 %% Called when the plugin application stop
